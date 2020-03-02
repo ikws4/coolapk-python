9ed42d58720d90d83bf980e9d3c8694b54af90b6
ikws4 <zhipingne@gmail.com>
2020-03-02 19:36:54 +0800

    Optimization api request

--- api/api.py
+++ api/api.py
@@ -1,21 +1,41 @@
 import requests
 import random
 from .helper import get_request_hash, get_app_token
-from .models import User
+from .models import User, Banner
 
 
-def login(u: str, p: str) -> User:
-    """
-    模拟酷安登录
-
-    通过 cookies 获取 token username uid 转为 User 对象
+def _base_post(url, **kwargs):
+    """POST请求封装
 
     Args:
-        u: 用户名、邮箱、手机号
-        p: 密码
+        url (str): url.
+        **kwargs:  **kwargs for `request.post`.
 
     Returns:
-        User: 用户信息
+        requests.Response: 请求返回体.
+
+    """
+    url = 'https://api.coolapk.com' + url
+    headers = {
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-App-Id': 'com.coolapk.market',
+        'X-App-Token': get_app_token()
+    }
+    res = requests.post(url, headers=headers, **kwargs)
+    return res
+
+
+def login(u, p):
+    """模拟酷安登录
+    通过 cookies 获取 token username uid 转为 `User` 对象
+
+    Args:
+        u (str): 用户名、手机号、邮箱
+        p (str): 密码
+
+    Returns:
+        User
+
     """
     url = 'https://account.coolapk.com/auth/loginByCoolAPk'
     res = requests.get(url)
@@ -37,33 +57,25 @@ def login(u: str, p: str) -> User:
     return User(**res.cookies.get_dict())
 
 
-def upload_avatar(user: User, image) -> bool:
-    """
-    上传头像
+def upload_avatar(uses, image):
+    """上传头像
 
     Args:
-        user: 用户对象
+        user (User): 用户对象
         image: 图片路径 或 字节数据
 
     Returns:
         bool: 上传成功返回 True
+
     """
-
-    url = 'https://api.coolapk.com/v6/account/changeAvatar'
-
-    headers = {
-        'X-Requested-With': 'XMLHttpRequest',
-        'X-App-Id': 'com.coolapk.market',
-        'X-App-Token': get_app_token()
-    }
-
     if isinstance(image, str):
         image = open(image, 'rb')
 
     files = {
         'imgFile': (str(random.random()), image, 'image/jpeg')
     }
-    res = requests.post(url, headers=headers, cookies=user.__dict__, files=files)
+    res = _base_post('/v6/account/changeAvatar',
+                     cookies=user.__dict__, files=files)
 
     if res.json().get('message'):
         return False
